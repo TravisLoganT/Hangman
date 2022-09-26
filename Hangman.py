d7823f6635c73472b6f18743ed06fe3fb926626e
Travis Logan <c1014427@newcastle.ac.uk>
2022-09-26 18:35:51 +0100

    created the boilerplate template of the code, with implementations of the turtle library

--- Hangman.py
+++ Hangman.py
@@ -4,10 +4,66 @@ import turtle
 import time
 import time
 
+# Variable Declarations for hangman
+win_flag = False
+lose_flag = False
+flag_a = False
+flag_wr = False
+game_flag = True
+incorrect_count = 0
+hmTC = (0, 0)
+hm = turtle.Turtle()
+hm.speed(0)
+state = turtle.Turtle()
+state.ht()
 
-def placeholder():
-    print()
+
+def is_correct():
+    state.clear()
+    state.color("green")
+    state.write("This is correct!", font=("Arial", 50, "bold"))
+
+
+def lost():
+    """
+    state.clear()
+    state.color("white")
+    state.write("You have lost!", font=("Arial", 50, "bold"))
+    xy_coordinates = letter[0].pos()
+    temp = turtle.Turtle()
+    temp.ht()
+    temp.speed()
+    temp.color("white")
+    temp.penup()
+    temp.setpos(xy_coordinates[0], xy_coordinates[1] + 40)
+    temp.pendown()
+    temp.write("this was the Movie:", align="left", font=("Arial", 30, "bold"))
+
+    for i in range(len(movie)):
+        if movie[i] not in user_list_correct):
+            letter[i].color("red")
+            letter[i].write(movie[i], align="left", font=("Arial", 20, "bold"))
+
+    """
+
+
+def winning():
+    state.clear()
+    state.color("white")
+    state.write("You have won!", font=("Arial", 50, "bold"))
+
+
+def already_used():
+    state.clear()
+    state.color("green")
+    state.write("You have already pressed that key!", font=("Arial", 50, "bold"))
+
+
+def is_wrong():
+    state.clear()
+    state.color("red")
+    state.write("This is wrong", font=("Arial", 50, "bold"))
 
 
 if __name__ == '__main__':
-    placeholder()
+    winning()
