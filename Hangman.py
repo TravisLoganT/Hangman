099c6a3019945c8085ee386922cc079394dde485
Travis Logan <c1014427@newcastle.ac.uk>
2022-09-26 18:04:08 +0100

    starting template, with chosen libraries

---
+++ Hangman.py
@@ -0,0 +1,13 @@
+# Possible Libraries that will be needed
+import sys
+import turtle
+import time
+import time
+
+
+def placeholder():
+    print()
+
+
+if __name__ == '__main__':
+    placeholder()
